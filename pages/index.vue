d5456adb1b3b29174385b4f99319a78723ecdf57
Ana  J Hall <anajhall@Tests-MacBook-Air.local>
2017-11-23 18:11:59 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">AnaBaNaNa Here</h1>
+      <h2 class="subtitle">I am a weaver and psychic cat whisperer, based is Monte Carlo. </h2>
+    </div>
+  </section>
+</template>
